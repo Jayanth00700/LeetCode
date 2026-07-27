39cceb9e220ae25cbdef446204dfdab24cc141f2
Jayanth00700 <ajayanth070@gmail.com>
2026-07-27 10:28:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (21.34%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -4,11 +4,9 @@ public:
         int i = 0;
         int n = s.length();
 
-        // Skip leading spaces
         while (i < n && s[i] == ' ')
             i++;
 
-        // Check sign
         int sign = 1;
         if (i < n && (s[i] == '+' || s[i] == '-')) {
             if (s[i] == '-')
@@ -18,12 +16,10 @@ public:
 
         long long result = 0;
 
-        // Read digits
         while (i < n && isdigit(s[i])) {
 
             result = result * 10 + (s[i] - '0');
 
-            // Handle overflow
             if (sign * result >= INT_MAX)
                 return INT_MAX;
 
