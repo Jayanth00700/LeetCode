b44f554f6a84be669f9f8f1f5b2ad55add0e010c
Jayanth00700 <ajayanth070@gmail.com>
2026-08-23 23:54:15 +0530

    Time: 4 ms (47.55%) | Memory: 14 MB (67.83%) - LeetSync

---
+++ 2039-sum-game/sum-game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int half = n / 2;
+
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            if (num[i] == '?')
+                leftQ++;
+            else
+                leftSum += num[i] - '0';
+        }
+
+        for (int i = half; i < n; i++) {
+            if (num[i] == '?')
+                rightQ++;
+            else
+                rightSum += num[i] - '0';
+        }
+
+        int diff = leftSum - rightSum;
+        int qDiff = leftQ - rightQ;
+
+        if (qDiff % 2 != 0)
+            return true;
+
+        return diff != -(qDiff / 2) * 9;
+    }
+};
